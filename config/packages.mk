a650dc22bd257ee4fb5c0b9c5f44e56db7c6ac16
Joshua Primero <jprimero155@gmail.com>
2018-12-21 01:59:48 +0000

    vendor: build some apps
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -1,6 +1,10 @@
 # Packages
 PRODUCT_PACKAGES += \
+    Gallery2 \
+    Phonograph \
+    Snap \
     WallpaperPicker
+
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
